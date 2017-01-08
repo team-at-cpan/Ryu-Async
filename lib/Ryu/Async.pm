cb71df02502e18ecd152f446da50e9239d90ac7b
Tom Molesworth <TEAM@cpan.org>
2017-01-08 21:46:36 +0800

    Retab

--- lib/Ryu/Async.pm
+++ lib/Ryu/Async.pm
@@ -32,60 +32,60 @@ use curry::weak;
 =cut
 
 sub from {
-	use Scalar::Util qw(blessed);
-	use namespace::clean qw(blessed);
-	my $self = shift;
+    use Scalar::Util qw(blessed);
+    use namespace::clean qw(blessed);
+    my $self = shift;
 
-	my $src = $self->source(label => 'from');
-	if(my $class = blessed $_[0]) {
-		if($class->isa('IO::Async::Stream')) {
-			my $stream = shift;
-			$stream->configure(
-				on_read => sub {
-					my ($stream, $buffref, $eof) = @_;
-					my $data = substr $$buffref, 0, length $$buffref, '';
-					$src->emit($data);
-					$src->finish if $eof && !$src->completed->is_ready;
-				}
-			);
-			unless($stream->parent) {
-				$self->add_child($stream);
-				$src->completed->on_ready(sub {
-					$self->remove_child($stream) if $stream->parent;
-				});
-			}
-			return $src;
-		} else {
-			die "whatevs";
-		}
-	} elsif(my $ref = ref $_[0]) {
-		if($ref eq 'ARRAY') {
-			my @pending = @{$_[0]};
-			(sub {
-				return unless @pending;
-				$src->emit(shift @pending);
-				$self->loop->later(__SUB__);
-			})->();
-		} else {
-			die "Unknown type $ref"
-		}
-	}
+    my $src = $self->source(label => 'from');
+    if(my $class = blessed $_[0]) {
+        if($class->isa('IO::Async::Stream')) {
+            my $stream = shift;
+            $stream->configure(
+                on_read => sub {
+                    my ($stream, $buffref, $eof) = @_;
+                    my $data = substr $$buffref, 0, length $$buffref, '';
+                    $src->emit($data);
+                    $src->finish if $eof && !$src->completed->is_ready;
+                }
+            );
+            unless($stream->parent) {
+                $self->add_child($stream);
+                $src->completed->on_ready(sub {
+                    $self->remove_child($stream) if $stream->parent;
+                });
+            }
+            return $src;
+        } else {
+            die "whatevs";
+        }
+    } elsif(my $ref = ref $_[0]) {
+        if($ref eq 'ARRAY') {
+            my @pending = @{$_[0]};
+            (sub {
+                return unless @pending;
+                $src->emit(shift @pending);
+                $self->loop->later(__SUB__);
+            })->();
+        } else {
+            die "Unknown type $ref"
+        }
+    }
 
-	my %args = @_;
-	if(my $dir = $args{directory}) {
-		opendir my $handle, $dir or die $!;
-		(sub {
-			if(defined(my $item = readdir $handle)) {
-				$src->emit($item) unless $item eq '.' or $item eq '..';
-				$self->loop->later(__SUB__);
-			} else {
-				closedir $handle or die $!;
-				$src->finish
-			}
-		})->();
-		return $self;
-	}
-	die "unknown stuff";
+    my %args = @_;
+    if(my $dir = $args{directory}) {
+        opendir my $handle, $dir or die $!;
+        (sub {
+            if(defined(my $item = readdir $handle)) {
+                $src->emit($item) unless $item eq '.' or $item eq '..';
+                $self->loop->later(__SUB__);
+            } else {
+                closedir $handle or die $!;
+                $src->finish
+            }
+        })->();
+        return $self;
+    }
+    die "unknown stuff";
 }
 
 =head2 timer
@@ -93,26 +93,26 @@ sub from {
 =cut
 
 sub timer {
-	my ($self, %args) = @_;
-	my $src = $self->source(label => 'timer');
-	my $code = $src->curry::weak::emit(1);
-	$self->add_child(
-		my $timer = IO::Async::Timer::Periodic->new(
-			%args,
-			on_tick => sub {
-				$code->();
-			},
-		)
-	);
-	Scalar::Util::weaken($timer);
-	$src->on_ready($self->_capture_weakself(sub {
-		my ($self) = @_;
-		return unless $timer;
-		$timer->stop if $timer->is_running;
-		$self->remove_child($timer)
-	}));
-	$timer->start;
-	$src
+    my ($self, %args) = @_;
+    my $src = $self->source(label => 'timer');
+    my $code = $src->curry::weak::emit(1);
+    $self->add_child(
+        my $timer = IO::Async::Timer::Periodic->new(
+            %args,
+            on_tick => sub {
+                $code->();
+            },
+        )
+    );
+    Scalar::Util::weaken($timer);
+    $src->on_ready($self->_capture_weakself(sub {
+        my ($self) = @_;
+        return unless $timer;
+        $timer->stop if $timer->is_running;
+        $self->remove_child($timer)
+    }));
+    $timer->start;
+    $src
 }
 
 =head2 source
@@ -122,16 +122,16 @@ Returns a new L<Ryu::Source> instance.
 =cut
 
 sub source {
-	my ($self, %args) = @_;
-	my $label = delete $args{label};
-	$label //= (caller 1)[3];
+    my ($self, %args) = @_;
+    my $label = delete $args{label};
+    $label //= (caller 1)[3];
     $label =~ s/^Net::Async::/Na/g;
     $label =~ s/::([^:]*)$/->$1/;
-	Ryu::Source->new(
-		new_future => $self->loop->curry::weak::new_future,
-		label      => $label,
-		%args,
-	)
+    Ryu::Source->new(
+        new_future => $self->loop->curry::weak::new_future,
+        label      => $label,
+        %args,
+    )
 }
 
 1;
